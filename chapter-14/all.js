fdcaf55aa5585e1f12914b22f335bf16030cc051
BulbCat.David <tiida541108@gmail.com>
2018-11-03 14:30:02 +0800

    [ADD] chapter-14 ajax post demo
    add chapter-14 ajax post demo

--- chapter-14/all.js
+++ chapter-14/all.js
@@ -1,9 +1,28 @@
-var account = {
-	email: 'qqeuieeewfff@hexschool.com',
-	password:'1234'
+
+var send = document.querySelector('.send');
+
+send.addEventListener('click',signup,false);
+
+function signup(){
+    var emailStr = document.querySelector('.account').value;
+    var passwordStr = document.querySelector('.password').value;
+    var account = {};
+    account.email = emailStr;
+    account.password = passwordStr;
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signup',true);
+    xhr.setRequestHeader('Content-type','application/json');
+    var data = JSON.stringify(account);
+    xhr.send(data);
+    xhr.onload = function(){
+        var callbackData = JSON.parse(xhr.responseText);
+        console.log(callbackData);
+        var veriStr =  callbackData.message;
+        if(veriStr =="帳號註冊成功"){
+            alert('帳號註冊成功！！');
+        }else{
+            alert("帳號註冊失敗！");
+        }
+    }
 }
-var xhr = new XMLHttpRequest();
-xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signup',true);
-xhr.setRequestHeader('Content-type','application/json');
-var data = JSON.stringify(account);
-xhr.send(data);
